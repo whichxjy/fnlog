46ed86a64fff32cd9e3b6c014141613fac444640
whichxjy <whichxjy@gmail.com>
2020-11-24 04:18:31 +0800

    Add examples

--- examples/main.rs
+++ examples/main.rs
@@ -1,5 +1,19 @@
-use fnlog::hello;
+use fnlog::{fn_debug, fn_error, fn_info, fn_trace, fn_warn};
+
+fn hello() {
+    fn_trace!("trace in hello");
+    fn_debug!("debug in hello");
+    fn_info!("info in hello");
+    fn_warn!("warn in hello");
+    fn_error!("error in hello");
+}
 
 fn main() {
+    env_logger::init();
+    fn_trace!("trace in main");
+    fn_debug!("debug in main");
+    fn_info!("info in main");
+    fn_warn!("warn in main");
+    fn_error!("error in main");
     hello();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
-pub fn hello() {
-    println!("Hello, world!");
+#[macro_export]
+macro_rules! fn_trace {
+    ($x:expr $(, $($y:expr),+)?) => {
+        log::trace!(concat!("[{}] ", $x), stdext::function_name!() $(, $($y),+)?);
+    };
+}
+
+#[macro_export]
+macro_rules! fn_debug {
+    ($x:expr $(, $($y:expr),+)?) => {
+        log::debug!(concat!("[{}] ", $x), stdext::function_name!() $(, $($y),+)?);
+    };
+}
+
+#[macro_export]
+macro_rules! fn_info {
+    ($x:expr $(, $($y:expr),+)?) => {
+        log::info!(concat!("[{}] ", $x), stdext::function_name!() $(, $($y),+)?);
+    };
+}
+
+#[macro_export]
+macro_rules! fn_warn {
+    ($x:expr $(, $($y:expr),+)?) => {
+        log::warn!(concat!("[{}] ", $x), stdext::function_name!() $(, $($y),+)?);
+    };
+}
+
+#[macro_export]
+macro_rules! fn_error {
+    ($x:expr $(, $($y:expr),+)?) => {
+        log::error!(concat!("[{}] ", $x), stdext::function_name!() $(, $($y),+)?);
+    };
 }
